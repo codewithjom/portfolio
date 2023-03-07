f628eff9dd1b25385a7cc5abc267edc86d524c9c
Jom Dollesin <codewithjom@gmail.com>
2023-03-07 09:53:41 +0800

    feat: initial commit.

---
+++ components/Nav.tsx
@@ -0,0 +1,18 @@
+import { Fira_Code, Londrina_Solid } from '@next/font/google'
+import Link from 'next/link'
+
+const firaCode = Fira_Code({ subsets: ['latin'] })
+const londrina = Londrina_Solid({ weight: '400', subsets: ['latin']})
+
+export default function Nav() {
+  return (
+    <nav className='flex justify-between py-2 shadow-md'>
+      <h1 className={`text-2xl ${londrina.className} lg:pl-52 md:pl-24 pl-12`}>codewithjom</h1>
+      <ul className={`text-sm ${firaCode.className} lg:flex md:flex lg:pr-52 md:pr-24 py-2 lg:gap-6 md:gap-4 hidden`}>
+        <li>
+          <Link href='#' className='rounded py-2 px-4 bg-teal-700 text-stone-200'>Resume</Link>
+        </li>
+      </ul>
+    </nav>
+  )
+}
