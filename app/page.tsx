e06ef4aab4b77286a9a2bd7ed2e888dd76e846fb
Jom Dollesin <codewithjom@gmail.com>
2023-03-13 18:00:29 +0800

    feat: added footer.

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,7 @@
 import Nav from '@/components/Nav'
 import Intro from '@/components/Intro'
 import Projects from '@/components/Projects'
+import Footer from '@/components/Footer'
 
 export default function Home() {
   return (
@@ -8,6 +9,7 @@ export default function Home() {
       <Nav />
       <Intro />
       <Projects />
+      <Footer />
     </main>
   )
 }
---
+++ components/Footer.tsx
@@ -0,0 +1,7 @@
+export default function Footer() {
+  return (
+    <div className="flex justify-center pt-32 pb-5 text-sm opacity-50">
+      <p>&copy; {new Date().getFullYear()} Jom Dollesin. All Rights Reserved.</p>
+    </div>
+  )
+}
